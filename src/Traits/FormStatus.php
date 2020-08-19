cf121b3416d2ca89e12c8cf5a7522608f55a9847
Stephen Neal <sfneal@users.noreply.github.com>
2020-08-19 16:35:44 +0000

    Apply fixes from StyleCI

--- src/Traits/FormStatus.php
+++ src/Traits/FormStatus.php
@@ -1,38 +1,43 @@
 <?php
 
-
 namespace Sfneal\ViewModels\Traits;
 
 trait FormStatus
 {
     /**
-     * Add success status to the view model
+     * Add success status to the view model.
      *
      * @return $this
      */
-    public function withSuccess() {
+    public function withSuccess()
+    {
         $this->withStatus('success');
+
         return $this;
     }
 
     /**
-     * Add success status to the view model
+     * Add success status to the view model.
      *
      * @return $this
      */
-    public function withFailure() {
+    public function withFailure()
+    {
         $this->withStatus('failure');
+
         return $this;
     }
 
     /**
-     * Set a Client Inquiry success/failure status to be displayed in the view model
+     * Set a Client Inquiry success/failure status to be displayed in the view model.
      *
      * @param string $status
      * @return $this
      */
-    private function withStatus(string $status) {
+    private function withStatus(string $status)
+    {
         $this->status = $status;
+
         return $this;
     }
 }
