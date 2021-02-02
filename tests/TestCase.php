20e0c232e4d431c5442847277b11b1b2d8afe0a3
Stephen Neal <stephen.neal14@gmail.com>
2021-02-02 14:03:27 -0500

    ADD registering of Redis package providers & app configs

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,24 +2,68 @@
 
 namespace Sfneal\ViewModels\Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Lunaweb\RedisMock\Providers\RedisMockServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Sfneal\Helpers\Redis\Providers\RedisHelpersServiceProvider;
+use Sfneal\Helpers\Redis\RedisCache;
 use Spatie\ViewModels\Providers\ViewModelsServiceProvider;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestCase extends OrchestraTestCase
 {
-    protected function getPackageProviders($app)
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
     {
-        return [ViewModelsServiceProvider::class];
+        $app['config']->set('app.debug', true);
+        $app['config']->set('database.redis.client', 'mock');
+        $app['config']->set('cache.default', 'redis');
+        $app['config']->set('cache.prefix', 'redis-helpers');
     }
 
+    /**
+     * Register package service providers.
+     *
+     * @param Application $app
+     * @return array|string
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            RedisHelpersServiceProvider::class,
+            RedisMockServiceProvider::class,
+            ViewModelsServiceProvider::class
+        ];
+    }
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
     protected function setUp(): void
     {
         parent::setUp();
 
         View::addLocation(__DIR__.'/resources/views');
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        RedisCache::flush();
+        parent::tearDown();
     }
 
     protected function createRequest(array $headers = []): Request
