6b7de71cbc14f1a0657f0c982eb04ca277b38524
Stephen Neal <stephen.neal14@gmail.com>
2021-02-02 15:28:59 -0500

    ADD test_caching_multiple_views method

--- tests/ViewModelViewsTest.php
+++ tests/ViewModelViewsTest.php
@@ -2,7 +2,6 @@
 
 namespace Sfneal\ViewModels\Tests;
 
-use Sfneal\Helpers\Redis\RedisCache;
 use Sfneal\ViewModels\Tests\Mocks\TestViewModel;
 
 class ViewModelViewsTest extends TestCase
@@ -17,19 +16,35 @@ class ViewModelViewsTest extends TestCase
         parent::setUp();
 
         $this->viewModels = [
-            (new TestViewModel())->view('test'),
-            (new TestViewModel())->view('test2'),
-            (new TestViewModel())->view('test.test'),
+            0 => (new TestViewModel())->view('test'),
+            1 => (new TestViewModel())->view('test')->setRedisKey('http://localhost?page=1'),
+            2 => (new TestViewModel())->view('test')->setRedisKey('http://localhost?page=2'),
+            3 => (new TestViewModel())->view('test2'),
+            4 => (new TestViewModel())->view('test2')->setRedisKey('http://localhost?page=1'),
+            5 => (new TestViewModel())->view('test2')->setRedisKey('http://localhost?page=2'),
+            6 => (new TestViewModel())->view('test.test'),
+            7 => (new TestViewModel())->view('test.test')->setRedisKey('http://localhost?page=1'),
+            8 => (new TestViewModel())->view('test.test')->setRedisKey('http://localhost?page=2'),
         ];
 
         foreach ($this->viewModels as $viewModel) {
             $viewModel->render();
         }
     }
-    public function test_multiple_views_are_cache()
+
+    public function test_caching_multiple_views()
     {
         foreach ($this->viewModels as $viewModel) {
             $this->assertTrue($viewModel->isCached());
         }
+    }
+
+    public function test_deleting_multiple_views()
+    {
+        $this->viewModels[3]->invalidateCache();
+
+        $this->assertFalse($this->viewModels[3]->isCached());
+        $this->assertFalse($this->viewModels[4]->isCached());
+        $this->assertFalse($this->viewModels[5]->isCached());
     }
 }
