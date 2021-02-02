2b4bcdb3371bf778b3f34a69008a894a94b1e089
Stephen Neal <stephen.neal14@gmail.com>
2021-02-02 14:53:19 -0500

    ADD cache invalidation & custom redis key setting tests

--- tests/ViewModelTest.php
+++ tests/ViewModelTest.php
@@ -48,7 +48,13 @@ class ViewModelTest extends TestCase
     public function test_caching()
     {
         $this->viewModel->render();
+        $viewModel = (new TestViewModel())
+            ->view('test')
+            ->setRedisKey('http://localhost?page=1');
+        $viewModel->render();
+
         $this->assertTrue($this->viewModel->isCached());
+        $this->assertTrue($viewModel->isCached());
     }
 
     public function test_cache_invalidation()
@@ -58,11 +64,57 @@ class ViewModelTest extends TestCase
         $this->assertFalse($this->viewModel->isCached());
     }
 
+    public function test_cache_invalidation_nested()
+    {
+        $this->viewModel->render();
+
+        $viewModel = (new TestViewModel());
+        $viewModel
+            ->view('test')
+            ->setRedisKey('http://localhost?page=1');
+        $viewModel->render();
+
+        $viewModel->invalidateCache();
+
+        $this->assertFalse($this->viewModel->isCached());
+        $this->assertFalse($viewModel->isCached());
+    }
+
+    public function test_cache_invalidation_nested_delete_one()
+    {
+        $this->viewModel->render();
+
+        $viewModel = (new TestViewModel());
+        $viewModel
+            ->view('test')
+            ->setRedisKey('http://localhost?page=1');
+        $viewModel->render();
+
+        $viewModel->invalidateCache(false);
+
+        $this->assertFalse($viewModel->isCached());
+        $this->assertTrue($this->viewModel->isCached());
+    }
+
     public function test_cache_key()
     {
         $this->viewModel->render();
         $cacheKey = $this->viewModel->cacheKey();
         $this->assertIsString($cacheKey);
-        $this->assertEquals('views:test#0#http://localhost', $cacheKey);
+        $this->assertEquals('views:test:0:http://localhost', $cacheKey);
+    }
+
+    public function test_redis_key()
+    {
+        $this->viewModel->render();
+
+        $viewModel = (new TestViewModel());
+        $viewModel
+            ->view('test')
+            ->setRedisKey('http://localhost?page=1')
+            ->render();
+
+        $this->assertNotSame($this->viewModel->cacheKey(), $viewModel->cacheKey());
+        $this->assertTrue($viewModel->isCached());
     }
 }
