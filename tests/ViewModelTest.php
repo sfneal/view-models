bc953a9407c6d04867f33dedaad0098fa79796d9
Stephen Neal <stephen.neal14@gmail.com>
2021-02-02 14:25:20 -0500

    ADD test_cache_key method

--- tests/ViewModelTest.php
+++ tests/ViewModelTest.php
@@ -15,29 +15,30 @@ class ViewModelTest extends TestCase
         parent::setUp();
 
         $this->viewModel = new TestViewModel();
+        $this->viewModel->view('test');
     }
 
     public function test_rendering()
     {
-        $this->assertIsString($this->viewModel->render('test'));
+        $this->assertIsString($this->viewModel->render());
     }
 
     public function test_rendering_no_cache()
     {
-        $this->assertIsString($this->viewModel->renderNoCache('test'));
+        $this->assertIsString($this->viewModel->renderNoCache());
     }
 
     public function test_rendering_no_cache_vs_cache()
     {
         $this->assertEquals(
-            $this->viewModel->renderNoCache('test'),
-            $this->viewModel->render('test')
+            $this->viewModel->renderNoCache(),
+            $this->viewModel->render()
         );
     }
 
     public function test_rendering_ttl()
     {
-        $this->viewModel->render('test');
+        $this->viewModel->render();
         $ttl = $this->viewModel->getTTL();
 
         $this->assertIsInt($ttl);
@@ -46,14 +47,22 @@ class ViewModelTest extends TestCase
 
     public function test_caching()
     {
-        $this->viewModel->render('test');
+        $this->viewModel->render();
         $this->assertTrue($this->viewModel->isCached());
     }
 
     public function test_cache_invalidation()
     {
-        $this->viewModel->render('test');
+        $this->viewModel->render();
         $this->viewModel->invalidateCache();
         $this->assertFalse($this->viewModel->isCached());
+    }
+
+    public function test_cache_key()
+    {
+        $this->viewModel->render();
+        $cacheKey = $this->viewModel->cacheKey();
+        $this->assertIsString($cacheKey);
+        $this->assertEquals($cacheKey, 'views:test#0#http://localhost');
     }
 }
